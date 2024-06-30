0fe7b88019913783a3945b5937b86ec9d0fd36e7
Facu <facukopech@gmail.com>
2024-06-30 11:03:47 -0300

    added spinner component

---
+++ src/components/spinner.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../styles/spinner.css'
+
+const Spinner = () => {
+  return (
+    <div className="loader-container">
+      <div id="loader">
+        <div className="ls-particles ls-part-1"></div>
+        <div className="ls-particles ls-part-2"></div>
+        <div className="ls-particles ls-part-3"></div>
+        <div className="ls-particles ls-part-4"></div>
+        <div className="ls-particles ls-part-5"></div>
+        <div className="lightsaber ls-left ls-green"></div>
+        <div className="lightsaber ls-right ls-red"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Spinner

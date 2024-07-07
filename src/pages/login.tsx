79b53db507ec98deb8bc12f33cd380a7bb9466fe
Facu <facukopech@gmail.com>
2024-07-07 11:10:45 -0300

    integrated login page with register page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,13 +3,19 @@ import { useNavigate } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
 import '../styles/login.css'
 import { useLoading } from '../context/loadingContext';
+import Register from './register';
+import ResultPopup from '../components/popups/resultPopup';
 
-function Login() {
+const Login = ({ closePopup }: { closePopup: () => void }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showRegister, setShowRegister] = useState(false);
+  const [resultMessage, setResultMessage] = useState('');
+  const [showResultPopup, setShowResultPopup] = useState(false);
   const navigate = useNavigate();
   const [loginMessageError, setLoginMessageError] = useState('');
-  const { setLoading } = useLoading(); 
+  const [registerSuccessMessage, setRegisterResultMessage] = useState('');
+  const { setLoading } = useLoading();
 
   const handleLogin = async (e: any) => {
     e.preventDefault();
@@ -37,14 +43,23 @@ function Login() {
       console.log('Login successful:', decodedToken);
     } catch (error) {
       console.error('Error during login:', error);
-    }finally {
+    } finally {
       setLoading(false);
     }
   };
 
+  const handleLoginClick = (showRegister: boolean) => {
+    setShowRegister(showRegister);
+  }
+
+  const handleRegisterResult = (message: string) => {
+    setShowRegister(false);
+    setRegisterResultMessage(message);
+  }
+
   return (
     <div className='loginContainer'>
-      <div id="form-ui">
+      {!showRegister ? <div id="form-ui">
         <form action="" method="post" id="form" onSubmit={handleLogin}>
           <div className="div_popcorn_icon"></div>
           <div id="form-body">
@@ -52,6 +67,7 @@ function Login() {
               <div id="welcome-line-1">Movie Theater</div>
               <div id="welcome-line-2">Welcome Back!</div>
               <div className="message_error">{loginMessageError}</div>
+              <div className={ registerSuccessMessage.includes('successfully') ? "message_success" : "message_error"}>{registerSuccessMessage}</div>
             </div>
             <div className={loginMessageError.length > 0 ? "input-area-modified" : "input-area"}>
               <div className="form-inp">
@@ -67,16 +83,21 @@ function Login() {
               <button id="submit-button" type="submit">Login</button>
             </div>
             <div id="bar"></div>
-            <div className="div_home_button">
-              <button className='btnHome' onClick={() => navigate("/")} title='Home'>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="svgHome">
-                  <path className='pathHome' d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+            <div className="div_action_btns">
+              <button className='actionBtn' onClick={() => navigate("/")} title='Home'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="svgIcon">
+                  <path className='svg-path' d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                </svg>
+              </button>
+              <button className='actionBtn' onClick={() => [setShowRegister(true), setRegisterResultMessage(''), setLoginMessageError('')]} title='Home'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="svgIcon">
+                  <path className='svg-path' d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
                 </svg>
               </button>
             </div>
           </div>
         </form>
-      </div>
+      </div> : <Register onClick={handleLoginClick} onRegisterResult={handleRegisterResult}></Register>}
     </div>
   );
 }

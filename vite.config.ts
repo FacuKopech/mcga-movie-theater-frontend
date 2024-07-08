ae05e4adcc8720c62518c48352c42d71285368f1
Facu <facukopech@gmail.com>
2024-07-07 21:27:54 -0300

    modified env config

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react-swc';
 type ViteMode = 'dev' | 'prod' | string;
 
 export default ({ mode }: { mode: ViteMode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
+  
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};  
   return defineConfig({
     plugins: [react()],
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_BACK_END_URL,
+          target: process.env.VITE_BACK_END_URL,
           changeOrigin: true,
         },
       },

ccffc6c94217b1324e2894d4c8dbf451617cfe87
Facu <facukopech@gmail.com>
2024-07-08 21:06:40 -0300

    modified config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,23 @@ import { defineConfig } from 'vite';
 export default defineConfig(() => {
   return {
     define: {
-      __APP_ENV__: process.env.VITE_APP_BACKEND_URL,
+      __APP_ENV__: JSON.stringify(process.env.VITE_APP_BACKEND_URL),
     },
   };
 });
+
+// type ViteMode = 'dev' | 'prod' | string;
+
+// export default ({ mode }: { mode: ViteMode }) => {
+//   return defineConfig({
+//     plugins: [react()],
+//     server: {
+//       proxy: {
+//         '/api': {
+//           target: JSON.stringify(loadEnv(mode, process.cwd()).VITE_APP_BACKEND_URL),
+//           changeOrigin: true,
+//         },
+//       },
+//     },
+//   });
+// };

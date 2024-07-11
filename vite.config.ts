f7d6e9ca2b85ff694c3e6d61385c9c207685c807
Facu <facukopech@gmail.com>
2024-07-11 15:58:34 -0300

    configuration to navigate through routes in vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
 
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
     define: {
       __APP_ENV__: JSON.stringify(env.VITE_APP_BACKEND_URL),
+    },
+    plugins: [react()],
+    build: {
+      outDir: 'dist',
     },
   }
 })

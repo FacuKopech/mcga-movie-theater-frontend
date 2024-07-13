933d157136d1bd665272bb6f3b2cd997b8956a0e
Facu <facukopech@gmail.com>
2024-07-13 15:02:12 -0300

    added authentication check when refreshing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import Login from "./pages/login";
 import './App.css'
 import { useLoading, LoadingProvider } from "./context/loadingContext";
@@ -6,11 +6,34 @@ import Spinner from "./components/spinner";
 import HomePageWithProvider from "./pages/homePageWithProvider";
 import ErrorPage from "./pages/404ErrorPage";
 import PrivateRoute from "./components/PrivateRoute";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const { loading } = useLoading();
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_APP_BACKEND_URL}/api/check-auth`, {
+          credentials: 'include',
+        });
+        if (response.ok) {
+          setIsAuthenticated(true);
+        } else {
+          setIsAuthenticated(false);
+          navigate("/login");
+        }
+      } catch (error) {
+        console.error("Error checking authentication:", error);
+        setIsAuthenticated(false);
+        navigate("/login");
+      }
+    };
+    checkAuth();
+  }, [navigate]);
+
   return (
     <div className="div-app-container">
       {loading && <Spinner />}

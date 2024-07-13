ea56436f0978d06c6ad91a93ff697b7094a9a733
Facu <facukopech@gmail.com>
2024-07-13 16:05:07 -0300

    modified App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import { useEffect, useState } from "react";
 
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(() => {
-    return document.cookie.split(';').some((cookie) => cookie.trim().startsWith('token='));
+    return document.cookie.split(';').some((cookie) => cookie.trim().startsWith('token'));
   });
   const { loading } = useLoading();
   const navigate = useNavigate();
